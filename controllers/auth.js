e5286349f1d963ff201299135e7f06bc18d5bd85
JohnGCDev <davis-jd@hotmail.com>
2021-04-07 12:52:55 -0500

    GoogleSignIn integrado

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const { response } = require("express");
 const bcryptjs = require("bcryptjs");
 const Usuario = require("../models/usuario");
 const { generarJWT } = require("../helpers/generar-jwt");
+const googleVerify = require("../helpers/google-verify");
 
 const login = async(req, res = response) => {
     const { correo, password } = req.body;
@@ -41,6 +42,47 @@ const login = async(req, res = response) => {
     }
 };
 
+const googleSignin = async(req, res = response) => {
+    try {
+        const { id_token } = req.body;
+        const { correo, nombre, img } = await googleVerify(id_token);
+
+        let usuario = await Usuario.findOne({ correo });
+        if (!usuario) {
+            // Crear usuario
+            const data = {
+                nombre,
+                correo,
+                password: ":P",
+                img,
+                google: true,
+            };
+            usuario = new Usuario(data);
+            await usuario.save();
+        }
+
+        if (!usuario.estado) {
+            // Usuario no existe en BD
+            return res.status(401).json({
+                msg: "Hable con el administrador, usuario bloqueado",
+            });
+        }
+
+        // Generar JWT
+        const token = await generarJWT(usuario.id);
+
+        res.json({
+            usuario,
+            token,
+        });
+    } catch (err) {
+        res.status(400).json({
+            msg: "Token de Google no es válido",
+        });
+    }
+};
+
 module.exports = {
     login,
+    googleSignin,
 };
